233dc00a2d33e6c80af941a042474e9faeb74d36
denimcouch <alexmata92@ymail.com>
2021-01-09 13:59:28 -0600

    navbar built and styled

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar () {
+  return (
+    <nav id="main-nav">
+      <div className="container">
+        <h1 className="l-heading"><span>713</span> Community</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Events</li>
+          <li>Submit</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
